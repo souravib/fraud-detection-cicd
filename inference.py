1db1a9550b21a05938417e8c89e5529a8fae5316
souravib <souravi.banerjee@gmail.com>
2025-05-28 06:23:51 +0100

    Add files

---
+++ inference.py
@@ -0,0 +1,19 @@
+
+import joblib
+import pandas as pd
+import io
+
+def model_fn(model_dir):
+    return joblib.load(f"{model_dir}/fraud_model.pkl")
+
+def input_fn(request_body, request_content_type):
+    if request_content_type == 'text/csv':
+        return pd.read_csv(io.StringIO(request_body), header=None)
+    else:
+        raise ValueError(f"Unsupported content type: {request_content_type}")
+
+def predict_fn(input_data, model):
+    return model.predict(input_data)
+
+def output_fn(prediction, content_type):
+    return str(prediction.tolist())
